31ae5bc7502f6e51f8ca03b1455a276416dec14d
Matthew Simoni <mattsimoni2d@gmail.com>
2025-05-02 13:23:25 -0400

    Initial Commit

---
+++ TakeHomeTestResturantApp/TakeHomeTestResturantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestResturantAppApp.swift
+//  TakeHomeTestResturantApp
+//
+//  Created by Wayne Simoni on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTestResturantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TakeHomeTestResturantAppTests/TakeHomeTestResturantAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestResturantAppTests.swift
+//  TakeHomeTestResturantAppTests
+//
+//  Created by Wayne Simoni on 5/2/25.
+//
+
+import Testing
+@testable import TakeHomeTestResturantApp
+
+struct TakeHomeTestResturantAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
